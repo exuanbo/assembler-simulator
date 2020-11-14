a2f3fa55ed90a582f11068bc3bf6d25baf421e89
exuanbo <exuanbo@protonmail.com>
2020-11-14 19:26:19 +0000

    build: add `preact-cli-entrypoint` alias

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,15 @@
+import path from 'path'
+
 export default {
   webpack(config, env) {
     if (env.production) {
       config.performance.hints = false
     }
+
+    // Use any `index` file, not just index.js
+    config.resolve.alias['preact-cli-entrypoint'] = path.resolve(
+      process.cwd(),
+      'src/index'
+    )
   }
 }
