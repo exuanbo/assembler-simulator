953c6963815d66f9d608851d1668bf3dccf7f566
exuanbo <exuanbo@protonmail.com>
2020-11-14 01:33:25 +0000

    chore: update `.eslintrc.js`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
-module.exports = {
-  ...require('ts-standardx/.eslintrc.js'),
+const { mergeObj } = require('standard-engine-ts')
+
+module.exports = mergeObj(require('ts-standardx/.eslintrc.js'), {
   settings: {
     react: {
       pragma: 'h',
       version: 'detect'
     }
   }
-}
+})
