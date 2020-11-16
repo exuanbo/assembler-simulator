fae24d5c5de8c25ac18bebc7552abb01fae6a2dc
exuanbo <exuanbo@protonmail.com>
2020-11-16 03:53:58 +0000

    feat(assembler): add `getCompareOpcode`
    get jump opcode

--- src/utils/assembler.ts
+++ src/utils/assembler.ts
@@ -2,28 +2,13 @@ import { ParsedArg } from './parseArg'
 import {
   Keyword,
   ArithmeticKeyword,
-  ArgType,
-  OPCODE_MAPPING
+  StaticOpcodeKeyword,
+  OPCODE_MAPPING,
+  ArgType
 } from './constants'
 
 export const generateAddressArr = (withVDU: boolean): number[] =>
   [...Array(0x100)].map((_, index) => (withVDU && index >= 0xc0 ? 0x20 : 0x00))
-
-export const getArithmeticOpcode = (
-  token: ArithmeticKeyword,
-  dest: ParsedArg,
-  src: ParsedArg
-): number | undefined => {
-  switch (dest.type) {
-    case ArgType.Register:
-      switch (src.type) {
-        case ArgType.Register:
-          return (OPCODE_MAPPING[token] as [number, number])[0]
-        case ArgType.Number:
-          return (OPCODE_MAPPING[token] as [number, number])[1]
-      }
-  }
-}
 
 type MovOpcode = 0xd0 | 0xd1 | 0xd3 | 0xd2 | 0xd4 | undefined
 
@@ -53,18 +38,76 @@ export const getMovOpcode = (dest: ParsedArg, src: ParsedArg): MovOpcode => {
   }
 }
 
-export const getOpcode = (
-  token: Keyword,
+type GetOpcodeResult = number | undefined
+
+export const getArithmeticOpcode = (
+  token: ArithmeticKeyword,
   dest: ParsedArg,
   src: ParsedArg
-): number | undefined => {
+): GetOpcodeResult => {
+  switch (dest.type) {
+    case ArgType.Register:
+      switch (src.type) {
+        case ArgType.Register:
+          return (OPCODE_MAPPING[token] as [number, number])[0]
+        case ArgType.Number:
+          return (OPCODE_MAPPING[token] as [number, number])[1]
+      }
+  }
+}
+
+type CompareOpcode = 0xda | 0xdc | 0xdb | undefined
+
+export const getCompareOpcode = (
+  arg1: ParsedArg,
+  arg2: ParsedArg
+): CompareOpcode => {
+  switch (arg1.type) {
+    case ArgType.Register:
+      switch (arg2.type) {
+        case ArgType.Register:
+          return 0xda
+        case ArgType.Address:
+          return 0xdc
+        case ArgType.Number:
+          return 0xdb
+      }
+  }
+}
+
+const getStaticOpcode = (keyword: StaticOpcodeKeyword): number =>
+  OPCODE_MAPPING[Keyword[keyword]] as number
+
+export const getOpcode = (
+  token: Keyword | string,
+  arg1: ParsedArg,
+  arg2?: ParsedArg
+): GetOpcodeResult => {
+  if (arg2 === undefined) {
+    switch (token) {
+      case Keyword.INC:
+        return getStaticOpcode(Keyword.INC)
+      case Keyword.DEC:
+        return getStaticOpcode(Keyword.DEC)
+      case Keyword.JMP:
+        return getStaticOpcode(Keyword.JMP)
+      case Keyword.JZ:
+        return getStaticOpcode(Keyword.JZ)
+      case Keyword.JNZ:
+        return getStaticOpcode(Keyword.JNZ)
+    }
+    return
+  }
+
   switch (token) {
     case Keyword.MOV:
-      return getMovOpcode(dest, src)
+      return getMovOpcode(arg1, arg2)
     case Keyword.ADD:
     case Keyword.SUB:
     case Keyword.MUL:
     case Keyword.DIV:
-      return getArithmeticOpcode(token, dest, src)
+      return getArithmeticOpcode(token, arg1, arg2)
+    case Keyword.CMP:
+      return getCompareOpcode(arg1, arg2)
   }
 }
