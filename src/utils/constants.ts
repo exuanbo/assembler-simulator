5065ad7da625e720be2f743681b800161cbc2a61
exuanbo <exuanbo@protonmail.com>
2020-11-16 03:47:03 +0000

    refactor(constant): use non-const enum
    add `StaticOpcodeKeyword`

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const enum Keyword {
+export enum Keyword {
   MOV = 'MOV',
   ADD = 'ADD',
   SUB = 'SUB',
@@ -18,57 +18,15 @@ export type ArithmeticKeyword = Extract<
   Keyword.ADD | Keyword.SUB | Keyword.MUL | Keyword.DIV
 >
 
-type ArgsCount = 0 | 1 | 2
-
-export const ARGS_COUNT: { [keyword in Keyword]: ArgsCount } = {
-  [Keyword.MOV]: 2,
-  [Keyword.ADD]: 2,
-  [Keyword.SUB]: 2,
-  [Keyword.MUL]: 2,
-  [Keyword.DIV]: 2,
-  [Keyword.INC]: 1,
-  [Keyword.DEC]: 1,
-  [Keyword.CMP]: 2,
-  [Keyword.JMP]: 1,
-  [Keyword.JZ]: 1,
-  [Keyword.JNZ]: 1,
-  [Keyword.END]: 0
-}
-
-export const enum ArgType {
-  Number = 'Number',
-  Address = 'Address',
-  Register = 'Register',
-  RegisterPointer = 'RegisterPointer',
-  Illegal = 'Illegal'
-}
-
-type ValidArgTypeRegex = {
-  [argType in Exclude<ArgType, ArgType.Illegal>]: RegExp
-}
-
-export const ARG_TYPE_REGEX: ValidArgTypeRegex = {
-  [ArgType.Number]: /^([0-9A-F]{1,2})$/,
-  [ArgType.Address]: /^\[([0-9A-F]{1,2})\]$/,
-  [ArgType.Register]: /^([ABCD]L)$/,
-  [ArgType.RegisterPointer]: /^\[([ABCD]L)\]$/
-}
-
-export const enum Register {
-  AL = 'AL',
-  BL = 'BL',
-  CL = 'CL',
-  DL = 'DL'
-}
-
-export type RegisterCode = 0x00 | 0x01 | 0x02 | 0x03
-
-export const REGISTER_CODE: { [name in Register]: RegisterCode } = {
-  [Register.AL]: 0x00,
-  [Register.BL]: 0x01,
-  [Register.CL]: 0x02,
-  [Register.DL]: 0x03
-}
+export type StaticOpcodeKeyword = Extract<
+  Keyword,
+  | Keyword.INC
+  | Keyword.DEC
+  | Keyword.JMP
+  | Keyword.JZ
+  | Keyword.JNZ
+  | Keyword.END
+>
 
 type OpcodeMappingValue = [number, number] | number
 
@@ -87,4 +45,56 @@ export const OPCODE_MAPPING: OpcodeMapping = {
   [Keyword.JZ]: 0xc1,
   [Keyword.JNZ]: 0xc2,
   [Keyword.END]: 0x00
+}
+
+type ArgsCount = 0 | 1 | 2
+
+export const ARGS_COUNT: { [keyword in Keyword]: ArgsCount } = {
+  [Keyword.MOV]: 2,
+  [Keyword.ADD]: 2,
+  [Keyword.SUB]: 2,
+  [Keyword.MUL]: 2,
+  [Keyword.DIV]: 2,
+  [Keyword.INC]: 1,
+  [Keyword.DEC]: 1,
+  [Keyword.CMP]: 2,
+  [Keyword.JMP]: 1,
+  [Keyword.JZ]: 1,
+  [Keyword.JNZ]: 1,
+  [Keyword.END]: 0
+}
+
+export enum ArgType {
+  Number = 'Number',
+  Address = 'Address',
+  Register = 'Register',
+  RegisterPointer = 'RegisterPointer',
+  Illegal = 'Illegal'
+}
+
+type ValidArgTypeRegex = {
+  [argType in Exclude<ArgType, ArgType.Illegal>]: RegExp
+}
+
+export const ARG_TYPE_REGEX: ValidArgTypeRegex = {
+  [ArgType.Number]: /^([0-9A-F]{1,2})$/,
+  [ArgType.Address]: /^\[([0-9A-F]{1,2})\]$/,
+  [ArgType.Register]: /^([ABCD]L)$/,
+  [ArgType.RegisterPointer]: /^\[([ABCD]L)\]$/
+}
+
+export enum Register {
+  AL = 'AL',
+  BL = 'BL',
+  CL = 'CL',
+  DL = 'DL'
+}
+
+export type RegisterCode = 0x00 | 0x01 | 0x02 | 0x03
+
+export const REGISTER_CODE: { [name in Register]: RegisterCode } = {
+  [Register.AL]: 0x00,
+  [Register.BL]: 0x01,
+  [Register.CL]: 0x02,
+  [Register.DL]: 0x03
 }
