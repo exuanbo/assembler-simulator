e64b8da09f44b90665b1cbd6828506827e044d7a
exuanbo <exuanbo@protonmail.com>
2020-11-19 13:45:06 +0000

    chore(deps-dev): rm less, less-loader

--- preact.config.js
+++ preact.config.js
@@ -8,15 +8,6 @@ export default {
       config.performance.hints = false
     }
 
-    /** https://github.com/preactjs/preact-cli/blob/master/packages/cli/lib/lib/webpack/webpack-base-config.js#L173 */
-    const lessLoader = helpers
-      .getLoadersByName(config, 'proxy-loader')
-      .filter(
-        loaderWrapper => loaderWrapper.loader.options.loader === 'less-loader'
-      )[0].loader.options
-
-    lessLoader.options.lessOptions.javascriptEnabled = true
-
     // Use any `index` file, not just index.js
     config.resolve.alias['preact-cli-entrypoint'] = path.resolve(
       process.cwd(),
