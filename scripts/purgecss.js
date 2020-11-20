252d62b948d15a5b4c132ceab2b2b11dc070cce4
exuanbo <exuanbo@protonmail.com>
2020-11-20 03:29:32 +0000

    build: add tsx files to purgecss content

--- scripts/purgecss.js
+++ scripts/purgecss.js
@@ -3,13 +3,21 @@ const path = require('path')
 const postcss = require('postcss')
 const purgecss = require('@fullhuman/postcss-purgecss')
 
-const buildpath = path.join(__dirname, '../build')
+const rootpath = process.cwd()
+const tsxpath = path.join(rootpath, 'src/components')
+const buildpath = path.join(rootpath, 'build')
+
 const filename = fs.readdirSync(buildpath).filter(fn => fn.endsWith('.css'))[0]
 const filepath = path.join(buildpath, filename)
 
 const css = fs.readFileSync(filepath, 'utf-8')
-const htmlpath = path.join(buildpath, 'index.html')
 
-postcss([purgecss({ content: [htmlpath] })])
-  .process(css, { from: filepath, to: filepath })
+const html = path.join(buildpath, 'index.html')
+const tsxArr = fs
+  .readdirSync(tsxpath)
+  .filter(fn => fn.endsWith('.tsx'))
+  .map(fn => path.join(tsxpath, fn))
+
+postcss([purgecss({ content: [html, ...tsxArr] })])
+  .process(css)
   .then(res => fs.writeFileSync(filepath, res.css))
