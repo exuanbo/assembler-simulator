6e56ebb1dc29a97792c6990d53bc6af922ecddcd
exuanbo <exuanbo@protonmail.com>
2020-11-27 13:25:59 +0000

    chore: rm react `version: 'detect'` in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,7 @@ const { mergeObj } = require('standard-engine-ts')
 module.exports = mergeObj(require('ts-standardx/.eslintrc.js'), {
   settings: {
     react: {
-      pragma: 'h',
-      version: 'detect'
+      pragma: 'h'
     }
   },
   rules: {
