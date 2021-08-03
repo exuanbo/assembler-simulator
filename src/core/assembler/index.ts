5c00be65a121ae0e9f5c490ae2508c0cb41df668
exuanbo <exuanbo@protonmail.com>
2021-08-03 11:29:33 +0100

    refactor(assembler): return `addressToOpcodeMap`

--- src/core/assembler/index.ts
+++ src/core/assembler/index.ts
@@ -5,57 +5,59 @@ import { DuplicateLabelError, AssembleError, LabelNotExistError } from './except
 import { Instruction } from '../constants'
 
 const getLabelToAddressMap = (statements: Statement[]): Map<string, number> => {
-  const [, labelAddressMap] = statements.reduce(
-    ([addressPointer, labelAddressMap], statement) => {
+  const [, labelToAddressMap] = statements.reduce(
+    ([address, labelToAddressMap], statement) => {
       const { label, instruction, operands, opcodes } = statement
       if (label !== null) {
-        if (labelAddressMap.has(label.identifier)) {
+        if (labelToAddressMap.has(label.identifier)) {
           throw new DuplicateLabelError(label)
         }
-        labelAddressMap.set(label.identifier, addressPointer)
+        labelToAddressMap.set(label.identifier, address)
       }
       const firstOperand = operands[0]
       if (instruction === Instruction.ORG) {
-        return [firstOperand.value as number, labelAddressMap]
+        return [firstOperand.value as number, labelToAddressMap]
       }
-      const nextAddressPointer =
-        addressPointer +
+      const nextAddress =
+        address +
         opcodes.length +
         (firstOperand !== undefined && firstOperand.type === OperandType.Label ? 1 : 0)
-      if (nextAddressPointer > 0xff) {
+      if (nextAddress > 0xff) {
         throw new AssembleError(statement)
       }
-      return [nextAddressPointer, labelAddressMap]
+      return [nextAddress, labelToAddressMap]
     },
     [0, new Map<string, number>()]
   )
-  return labelAddressMap
+  return labelToAddressMap
 }
 
-export const assemble = (input: string): [Uint8Array, Map<number, Statement>] => {
+export const assemble = (input: string): [Map<number, number>, Map<number, Statement>] => {
   const statements = parse(tokenize(input))
   const labelToAddressMap = getLabelToAddressMap(statements)
-  const [, data, addressToStatementMap] = statements.reduce(
-    ([addressPointer, data, addressToStatementMap], statement) => {
+  const [, addressToOpcodeMap, addressToStatementMap] = statements.reduce(
+    ([address, addressToOpcodeMap, addressToStatementMap], statement) => {
       const { instruction, operands, opcodes } = statement
       const firstOperand = operands[0]
       if (instruction === Instruction.ORG) {
-        return [firstOperand.value as number, data, addressToStatementMap]
+        return [firstOperand.value as number, addressToOpcodeMap, addressToStatementMap]
       }
       if (firstOperand !== undefined && firstOperand.type === OperandType.Label) {
         const labelAddress = labelToAddressMap.get(firstOperand.token.value)
         if (labelAddress === undefined) {
           throw new LabelNotExistError(firstOperand.token)
         }
-        const distance = labelAddress - addressPointer
+        const distance = labelAddress - address
         const unsignedDistance = distance < 0 ? 0x100 + distance : distance
         opcodes.push(unsignedDistance)
       }
-      data.set(opcodes, addressPointer)
-      addressToStatementMap.set(addressPointer, statement)
-      return [addressPointer + opcodes.length, data, addressToStatementMap]
+      opcodes.forEach((opcode, index) => {
+        addressToOpcodeMap.set(address + index, opcode)
+      })
+      addressToStatementMap.set(address, statement)
+      return [address + opcodes.length, addressToOpcodeMap, addressToStatementMap]
     },
-    [0, new Uint8Array(0x100), new Map<number, Statement>()]
+    [0, new Map<number, number>(), new Map<number, Statement>()]
   )
-  return [data, addressToStatementMap]
+  return [addressToOpcodeMap, addressToStatementMap]
 }
