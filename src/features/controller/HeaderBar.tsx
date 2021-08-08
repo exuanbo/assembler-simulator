aa1e1ac4e3c6daa984b29954bddb3bbd3a04f8e1
exuanbo <exuanbo@protonmail.com>
2021-08-08 22:19:51 +0100

    refactor: minor adjust `HeaderBar` style

--- src/features/controller/HeaderBar.tsx
+++ src/features/controller/HeaderBar.tsx
@@ -6,8 +6,8 @@ interface Props {
 }
 
 const HeaderBar = ({ className }: Props): JSX.Element => (
-  <nav className={`flex items-center justify-between h-10 px-3 bg-gray-50 ${className}`}>
-    <div className="flex space-x-3">
+  <nav className={`flex items-center justify-between h-10 bg-gray-50 ${className}`}>
+    <div className="flex h-full pl-2 space-x-3">
       <button className="flex items-center space-x-1 focus:outline-none">
         <Play aria-hidden="true" className="h-4 -ml-1" />
         <span>Run</span>
@@ -21,7 +21,7 @@ const HeaderBar = ({ className }: Props): JSX.Element => (
         <span>Reset</span>
       </button>
     </div>
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center pr-2 space-x-2">
       <span>Assembler Simulator</span>
       <a
         href="https://github.com/exuanbo/assembler-simulator"
