f8189938be8c0fd69983c271e7c4f32fdafb32db
exuanbo <exuanbo@protonmail.com>
2021-08-31 19:11:26 +0100

    feat: add signals to `cpuSlice`

--- src/features/cpu/cpuSlice.ts
+++ src/features/cpu/cpuSlice.ts
@@ -2,18 +2,58 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { CPU, init } from './core'
 import type { RootState } from '../../app/store'
 
-const initialState = init()
+type InputSignals = (
+  | {
+      data: number
+      inputPort: number
+    }
+  | {
+      data: undefined
+      inputPort: undefined
+    }
+) & {
+  interrupt: boolean
+}
+
+interface CPUState {
+  internal: CPU
+  inputSignals: InputSignals
+}
+
+const initialState: CPUState = {
+  internal: init(),
+  inputSignals: {
+    data: undefined,
+    inputPort: undefined,
+    interrupt: false
+  }
+}
 
 export const cpuSlice = createSlice({
   name: 'cpu',
   initialState,
   reducers: {
-    setState: (_state, action: PayloadAction<CPU>) => action.payload
+    setCPU: (state, action: PayloadAction<CPU>) => {
+      state.internal = action.payload
+    },
+    setInput: (state, action: PayloadAction<{ data: number; inputPort: number }>) => {
+      const { data, inputPort } = action.payload
+      state.inputSignals.data = data
+      state.inputSignals.inputPort = inputPort
+    },
+    clearInput: state => {
+      state.inputSignals.data = undefined
+      state.inputSignals.inputPort = undefined
+    },
+    setInterrupt: (state, action: PayloadAction<boolean>) => {
+      state.inputSignals.interrupt = action.payload
+    }
   }
 })
 
-export const selectCPU = (state: RootState): CPU => state.cpu
+export const selectCPU = (state: RootState): CPU => state.cpu.internal
+export const selectInputSignals = (state: RootState): InputSignals => state.cpu.inputSignals
 
-export const { setState } = cpuSlice.actions
+export const { setCPU, setInput, clearInput, setInterrupt } = cpuSlice.actions
 
 export default cpuSlice.reducer
