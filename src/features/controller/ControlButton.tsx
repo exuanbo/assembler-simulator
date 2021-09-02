d3910a24409ac67e5f77fabeaced1c9da94c2514
exuanbo <exuanbo@protonmail.com>
2021-09-02 17:39:50 +0100

    refactor(ui): `ControlButton` change hover color

--- src/features/controller/ControlButton.tsx
+++ src/features/controller/ControlButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ControlButton = ({ children, onClick }: Props): JSX.Element => (
   <button
-    className="flex space-x-1 py-1 px-2 items-center hover:(bg-light-blue-600 text-light-100 fill-light-100) focus:outline-none "
+    className="flex space-x-1 py-1 px-2 items-center hover:(bg-gray-200) focus:outline-none "
     onClick={onClick}>
     {children}
   </button>
