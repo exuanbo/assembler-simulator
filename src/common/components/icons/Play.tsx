8dfd12d1470f0cf0f8c2b165f44fcad4bba8ef07
exuanbo <exuanbo@protonmail.com>
2021-09-02 14:10:44 +0100

    refactor: rm unused attributes in svg icons

--- src/common/components/icons/Play.tsx
+++ src/common/components/icons/Play.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const Play = (props: Props): JSX.Element => (
-  <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M3 22v-20l18 10-18 10z" />
   </svg>
 )
