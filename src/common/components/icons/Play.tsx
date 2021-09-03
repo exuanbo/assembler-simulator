618f6d84deef7d5af63e959d3537465185f22ae2
exuanbo <exuanbo@protonmail.com>
2021-09-03 19:05:46 +0100

    chore: add source link to svg icons

--- src/common/components/icons/Play.tsx
+++ src/common/components/icons/Play.tsx
@@ -4,6 +4,9 @@ interface Props {
   [prop: string]: unknown
 }
 
+/**
+ * {@link https://iconmonstr.com/media-control-48-svg/}
+ */
 const Play = (props: Props): JSX.Element => (
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M3 22v-20l18 10-18 10z" />
--- src/common/components/icons/Redo.tsx
+++ src/common/components/icons/Redo.tsx
@@ -4,6 +4,9 @@ interface Props {
   [prop: string]: unknown
 }
 
+/**
+ * {@link https://iconmonstr.com/redo-4-svg/}
+ */
 const Redo = (props: Props): JSX.Element => (
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M6.974 22.957c-10.957-11.421 2.326-20.865 10.384-13.309l-2.464 2.352h9.106v-8.947l-2.232 2.229c-14.794-13.203-31.51 7.051-14.794 17.675z" />
