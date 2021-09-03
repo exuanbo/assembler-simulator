34a03c388271ca691e7e3babe1f0d3a7bc173b3d
exuanbo <exuanbo@protonmail.com>
2021-09-03 22:36:00 +0100

    fix(ui): `RunButton` text

--- src/features/controller/RunButton.tsx
+++ src/features/controller/RunButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ControlButton from './ControlButton'
 import { Play, Stop } from '../../common/components/icons'
+import { NO_BREAK_SPACE } from '../../common/constants'
 
 interface Props {
   getState: () => boolean
@@ -11,8 +12,17 @@ const RunButton = ({ getState, onClick }: Props): JSX.Element => {
   const isRunning = getState()
   return (
     <ControlButton onClick={onClick}>
-      {isRunning ? <Stop className="h-4" /> : <Play className="h-4" />}
-      <span>Run</span>
+      {isRunning ? (
+        <>
+          <Stop className="h-4" />
+          <span>Stop</span>
+        </>
+      ) : (
+        <>
+          <Play className="h-4" />
+          <span>Run{NO_BREAK_SPACE}</span>
+        </>
+      )}
     </ControlButton>
   )
 }
