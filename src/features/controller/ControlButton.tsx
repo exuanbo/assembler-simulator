a082f7c63a5841206223b8d29b435fbfffca42e0
exuanbo <exuanbo@protonmail.com>
2021-09-04 20:50:20 +0100

    refactor(ui): `ControlButton` add space between

--- src/features/controller/ControlButton.tsx
+++ src/features/controller/ControlButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ControlButton = ({ children, onClick }: Props): JSX.Element => (
   <button
-    className="flex space-x-1 py-1 px-2 items-center hover:(bg-gray-200) focus:outline-none "
+    className="flex space-x-2 py-1 px-2 items-center hover:bg-gray-200 focus:outline-none "
     onClick={onClick}>
     {children}
   </button>
