332bffa9b35859d051dbc92acaa36f754c230785
exuanbo <exuanbo@protonmail.com>
2021-09-04 14:16:58 +0100

    refactor(ui): minor modify `Card`

--- src/common/components/Card.tsx
+++ src/common/components/Card.tsx
@@ -4,16 +4,16 @@ interface Props {
   children: React.ReactNode
   title: string
   className?: string | undefined
-  Icon?: (() => JSX.Element) | undefined
+  Icon?: ((props: Record<string, unknown>) => JSX.Element) | undefined
   onIconClick?: (() => void) | undefined
 }
 
 const Card = ({ children, title, className, Icon, onIconClick }: Props): JSX.Element => (
   <div className={className}>
-    <header className="flex justify-between w-full px-3 py-1 mb-0.5 bg-gray-100 border-b shadow">
+    <header className="border-b flex bg-gray-100 shadow mb-0.5 py-1 px-2 items-center justify-between">
       <span>{title}</span>
       {Icon !== undefined ? (
-        <button className="flex flex-col justify-center focus:outline-none" onClick={onIconClick}>
+        <button className="flex items-center focus:outline-none" onClick={onIconClick}>
           <Icon />
         </button>
       ) : null}
