1c6804dbf1d704e365b234355176d7b762b9bef5
exuanbo <exuanbo@protonmail.com>
2021-09-13 21:11:32 +0100

    refactor: `initData` use `Array.from()`

--- src/features/memory/core.ts
+++ src/features/memory/core.ts
@@ -1,7 +1,7 @@
 import type { AddressToMachineCodeMap } from '../assembler/core'
 
 export const initData = (): number[] =>
-  [...Array(0x100)].map((_, address) => (address < 0xc0 ? 0 : 0x20))
+  Array.from({ length: 0x100 }, (_, address) => (address < 0xc0 ? 0 : 0x20))
 
 export const initDataFrom = (map: AddressToMachineCodeMap): number[] => {
   const data = initData()
