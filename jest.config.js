d6489b00b9ef6cfe44c1e1ac318c881d8a93fe99
exuanbo <exuanbo@protonmail.com>
2021-09-16 23:37:29 +0100

    chore: update coverage sources

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const FEATURES_DIR = 'src/features/'
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/common/*.ts',
+    'src/common/utils.ts',
     ...['assembler/core/*.ts', 'cpu/core/*.ts', 'memory/core.ts'].map(path => FEATURES_DIR + path)
   ],
   preset: 'ts-jest',
