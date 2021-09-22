3fd5633d2cb0bc0d5768680554bf3b4dacc92526
exuanbo <exuanbo@protonmail.com>
2021-09-22 19:36:17 +0100

    refactor(codemirror): extract `theme.ts`

--- src/features/editor/codemirror/setup.ts
+++ src/features/editor/codemirror/setup.ts
@@ -5,34 +5,15 @@ import { history, historyKeymap } from '@codemirror/history'
 import { lineNumbers } from '@codemirror/gutter'
 import { closeBrackets, closeBracketsKeymap } from '@codemirror/closebrackets'
 import { defaultHighlightStyle } from '@codemirror/highlight'
+import { theme } from './theme'
 import { wavyUnderline } from './wavyUnderline'
 import { highlightActiveRange } from './highlightActiveRange'
 import { asm } from './lang-asm'
 import { breakpoints } from './breakpoints'
 import { indentWithTab } from './indentWithTab'
 
-// TODO: extract to seprate file
-const theme = EditorView.theme({
-  '&': {
-    height: '100%',
-    width: '50vw'
-  },
-  '&.cm-focused': {
-    outline: '0'
-  },
-  '.cm-scroller': {
-    fontFamily: "'Jetbrains Mono', monospace"
-  },
-  '.cm-breakpoints': {
-    fontSize: '0.875em'
-  },
-  '.cm-lineNumbers': {
-    paddingRight: '6px'
-  }
-})
-
 export const setup: Extension = [
-  theme,
+  theme(),
   EditorView.lineWrapping,
   drawSelection(),
   wavyUnderline(),
---
+++ src/features/editor/codemirror/theme.ts
@@ -0,0 +1,22 @@
+import type { Extension } from '@codemirror/state'
+import { EditorView } from '@codemirror/view'
+
+export const theme = (): Extension =>
+  EditorView.theme({
+    '&': {
+      height: '100%',
+      width: '50vw'
+    },
+    '&.cm-focused': {
+      outline: '0'
+    },
+    '.cm-scroller': {
+      fontFamily: "'Jetbrains Mono', monospace"
+    },
+    '.cm-breakpoints': {
+      fontSize: '0.875em'
+    },
+    '.cm-lineNumbers': {
+      paddingRight: '6px'
+    }
+  })
