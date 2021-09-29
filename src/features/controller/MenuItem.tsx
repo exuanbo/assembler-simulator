90d06a1fefcacbd69469e08072a9ca857791d38c
exuanbo <exuanbo@protonmail.com>
2021-09-30 00:04:26 +0100

    refactor: `MenuItem.SubMenu`

--- src/features/controller/MenuItem.tsx
+++ src/features/controller/MenuItem.tsx
@@ -2,30 +2,27 @@ import React, { ReactNode, useState } from 'react'
 import { useHover } from '../../common/hooks'
 
 interface Props {
-  children: ReactNode | ((isHovered: boolean) => ReactNode)
+  children: ReactNode
+  innerRef?: React.Ref<HTMLDivElement>
   className?: string
   onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
-const MenuItem = ({ children, className = '', onClick }: Props): JSX.Element => {
-  const willHandleHover = typeof children === 'function'
-  const [isHovered, hoverRef] = useHover<HTMLDivElement>()
-
-  return (
-    <div
-      ref={willHandleHover ? hoverRef : undefined}
-      className={`flex py-1 px-2 items-center hover:bg-gray-200 ${className}`}
-      onClick={onClick}>
-      {willHandleHover ? children(isHovered) : children}
-    </div>
-  )
-}
+const MenuItem = ({ children, innerRef, className = '', onClick }: Props): JSX.Element => (
+  <div
+    ref={innerRef}
+    className={`flex py-1 px-2 items-center hover:bg-gray-200 ${className}`}
+    onClick={onClick}>
+    {children}
+  </div>
+)
 
 interface SubMenuProps {
   children: (isHovered: boolean, menuItemsRef: React.RefCallback<HTMLDivElement>) => ReactNode
 }
 
 MenuItem.SubMenu = ({ children }: SubMenuProps): JSX.Element => {
+  const [isHovered, hoverRef] = useHover<HTMLDivElement>()
   const [menuItems, setMenuItems] = useState<HTMLDivElement | null>(null)
 
   const handleClick = (event: React.MouseEvent): void => {
@@ -43,8 +40,8 @@ MenuItem.SubMenu = ({ children }: SubMenuProps): JSX.Element => {
   }
 
   return (
-    <MenuItem className="justify-between" onClick={handleClick}>
-      {isHovered => children(isHovered, menuItemsRef)}
+    <MenuItem className="justify-between" innerRef={hoverRef} onClick={handleClick}>
+      {children(isHovered, menuItemsRef)}
     </MenuItem>
   )
 }
