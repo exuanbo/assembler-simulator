7f2d08822bd9db52d0af347327846c79b956390c
exuanbo <exuanbo@protonmail.com>
2021-12-01 11:58:47 +0000

    refactor(ConfigurationMenu): inline components

--- src/features/controller/ConfigurationMenu.tsx
+++ src/features/controller/ConfigurationMenu.tsx
@@ -18,117 +18,118 @@ import {
   selectTimerInterval
 } from './controllerSlice'
 
-const AutoAssembleOption = (): JSX.Element => {
+const ConfigurationMenu = (): JSX.Element => {
   const dispatch = useDispatch()
-  const autoAssemble = useSelector(selectAutoAssemble)
+
+  const AutoAssemble = (): JSX.Element => {
+    const autoAssemble = useSelector(selectAutoAssemble)
+
+    return (
+      <MenuItem
+        onClick={() => {
+          dispatch(setAutoAssemble(!autoAssemble))
+        }}>
+        <MenuButton>
+          {autoAssemble ? <CheckMark /> : <span className="w-4" />}
+          <span>Auto Assemble</span>
+        </MenuButton>
+      </MenuItem>
+    )
+  }
+
+  const ClockSpeedMenu = (): JSX.Element => {
+    const clockSpeed = useSelector(selectClockSpeed)
+
+    return (
+      <MenuItem.Expandable>
+        {(isHovered, menuItemsRef) => (
+          <>
+            <MenuButton>
+              <span className="w-4" />
+              <span>Clock Speed</span>
+            </MenuButton>
+            {isHovered ? (
+              <MenuItems.Expanded className="mt-1px top-16" innerRef={menuItemsRef}>
+                {CLOCK_SPEED_KEYS.map((clockSpeedKey, index) => (
+                  <MenuItem
+                    key={index}
+                    onClick={() => {
+                      dispatch(setClockSpeed(ClockSpeed[clockSpeedKey]))
+                    }}>
+                    <MenuButton>
+                      {clockSpeed === ClockSpeed[clockSpeedKey] ? (
+                        <CheckMark />
+                      ) : (
+                        <span className="w-4" />
+                      )}
+                      <span>{clockSpeedKey}</span>
+                    </MenuButton>
+                  </MenuItem>
+                ))}
+              </MenuItems.Expanded>
+            ) : null}
+          </>
+        )}
+      </MenuItem.Expandable>
+    )
+  }
+
+  const TimerIntervalMenu = (): JSX.Element => {
+    const timerInterval = useSelector(selectTimerInterval)
+
+    return (
+      <MenuItem.Expandable>
+        {(isHovered, menuItemsRef) => (
+          <>
+            <MenuButton>
+              <span className="w-4" />
+              <span>Timer Interval</span>
+            </MenuButton>
+            {isHovered ? (
+              <MenuItems.Expanded className="mt-2px top-24" innerRef={menuItemsRef}>
+                {TIMER_INTERVAL_KEYS.map((timerIntervalKey, index) => (
+                  <MenuItem
+                    key={index}
+                    onClick={() => {
+                      dispatch(setTimerInterval(TimerInterval[timerIntervalKey]))
+                    }}>
+                    <MenuButton>
+                      {timerInterval === TimerInterval[timerIntervalKey] ? (
+                        <CheckMark />
+                      ) : (
+                        <span className="w-4" />
+                      )}
+                      <span>{timerIntervalKey}</span>
+                    </MenuButton>
+                  </MenuItem>
+                ))}
+              </MenuItems.Expanded>
+            ) : null}
+          </>
+        )}
+      </MenuItem.Expandable>
+    )
+  }
 
   return (
-    <MenuItem
-      onClick={() => {
-        dispatch(setAutoAssemble(!autoAssemble))
-      }}>
-      <MenuButton>
-        {autoAssemble ? <CheckMark /> : <span className="w-4" />}
-        <span>Auto Assemble</span>
-      </MenuButton>
-    </MenuItem>
-  )
-}
-
-const ClockSpeedMenu = (): JSX.Element => {
-  const dispatch = useDispatch()
-  const clockSpeed = useSelector(selectClockSpeed)
-
-  return (
-    <MenuItem.Expandable>
-      {(isHovered, menuItemsRef) => (
+    <Menu>
+      {isOpen => (
         <>
-          <MenuButton>
-            <span className="w-4" />
-            <span>Clock Speed</span>
-          </MenuButton>
-          {isHovered ? (
-            <MenuItems.Expanded className="mt-1px top-16" innerRef={menuItemsRef}>
-              {CLOCK_SPEED_KEYS.map((clockSpeedKey, index) => (
-                <MenuItem
-                  key={index}
-                  onClick={() => {
-                    dispatch(setClockSpeed(ClockSpeed[clockSpeedKey]))
-                  }}>
-                  <MenuButton>
-                    {clockSpeed === ClockSpeed[clockSpeedKey] ? (
-                      <CheckMark />
-                    ) : (
-                      <span className="w-4" />
-                    )}
-                    <span>{clockSpeedKey}</span>
-                  </MenuButton>
-                </MenuItem>
-              ))}
-            </MenuItems.Expanded>
+          <MenuButton.Main>
+            <Wrench />
+            <span>Configuration</span>
+          </MenuButton.Main>
+          {isOpen ? (
+            <MenuItems>
+              <AutoAssemble />
+              <ClockSpeedMenu />
+              <TimerIntervalMenu />
+            </MenuItems>
           ) : null}
         </>
       )}
-    </MenuItem.Expandable>
+    </Menu>
   )
 }
-
-const TimerIntervalMenu = (): JSX.Element => {
-  const dispatch = useDispatch()
-  const timerInterval = useSelector(selectTimerInterval)
-
-  return (
-    <MenuItem.Expandable>
-      {(isHovered, menuItemsRef) => (
-        <>
-          <MenuButton>
-            <span className="w-4" />
-            <span>Timer Interval</span>
-          </MenuButton>
-          {isHovered ? (
-            <MenuItems.Expanded className="mt-2px top-24" innerRef={menuItemsRef}>
-              {TIMER_INTERVAL_KEYS.map((timerIntervalKey, index) => (
-                <MenuItem
-                  key={index}
-                  onClick={() => {
-                    dispatch(setTimerInterval(TimerInterval[timerIntervalKey]))
-                  }}>
-                  <MenuButton>
-                    {timerInterval === TimerInterval[timerIntervalKey] ? (
-                      <CheckMark />
-                    ) : (
-                      <span className="w-4" />
-                    )}
-                    <span>{timerIntervalKey}</span>
-                  </MenuButton>
-                </MenuItem>
-              ))}
-            </MenuItems.Expanded>
-          ) : null}
-        </>
-      )}
-    </MenuItem.Expandable>
-  )
-}
-
-const ConfigurationMenu = (): JSX.Element => (
-  <Menu>
-    {isOpen => (
-      <>
-        <MenuButton.Main>
-          <Wrench />
-          <span>Configuration</span>
-        </MenuButton.Main>
-        {isOpen ? (
-          <MenuItems>
-            <AutoAssembleOption />
-            <ClockSpeedMenu />
-            <TimerIntervalMenu />
-          </MenuItems>
-        ) : null}
-      </>
-    )}
-  </Menu>
-)
 
 export default ConfigurationMenu
