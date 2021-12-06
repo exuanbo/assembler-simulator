f5e952ae40a30d6b48b9ec615d37dc46648d1c6c
exuanbo <exuanbo@protonmail.com>
2021-12-06 12:59:45 +0000

    refactor(codemirror/indentWithTab): use type-only import

--- src/features/editor/codemirror/indentWithTab.ts
+++ src/features/editor/codemirror/indentWithTab.ts
@@ -1,4 +1,4 @@
-import { KeyBinding } from '@codemirror/view'
+import type { KeyBinding } from '@codemirror/view'
 import { insertTab, indentLess } from '@codemirror/commands'
 
 export const indentWithTab: KeyBinding = {
