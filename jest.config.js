e08ea9fef90a7d3474a682fc71a1df2ceaafee46
exuanbo <exuanbo@protonmail.com>
2021-12-13 20:30:32 +0000

    chore: refactor jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
-const FEATURES_DIR = 'src/features/'
+const { join } = require('path')
+
+const FEATURES_DIR = 'src/features'
 
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     'src/common/utils.ts',
-    ...['assembler/core/*.ts', 'cpu/core/*.ts', 'memory/core.ts'].map(path => FEATURES_DIR + path)
+    ...['assembler/core/*.ts', 'cpu/core/*.ts', 'memory/core.ts'].map(pattern =>
+      join(FEATURES_DIR, pattern)
+    )
   ],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
