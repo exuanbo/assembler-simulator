8597a053459ff4c3d866e635c70cd119e41d213d
exuanbo <exuanbo@protonmail.com>
2022-01-01 00:42:38 +0000

    feat: disable `step` button if running

--- src/features/controller/ControlButtons.tsx
+++ src/features/controller/ControlButtons.tsx
@@ -8,10 +8,15 @@ import { NO_BREAK_SPACE } from '../../common/constants'
 interface Props {
   children: ReactNode
   onClick: React.MouseEventHandler<HTMLDivElement>
+  disabled?: boolean
 }
 
-const ControlButton = ({ children, onClick }: Props): JSX.Element => (
-  <div className="flex space-x-2 py-1 px-2 items-center hover:bg-gray-200" onClick={onClick}>
+const ControlButton = ({ children, onClick, disabled = false }: Props): JSX.Element => (
+  <div
+    className={`flex space-x-2 py-1 px-2 items-center ${
+      disabled ? 'text-gray-500 fill-gray-500' : 'hover:bg-gray-200'
+    }`}
+    onClick={onClick}>
     {children}
   </div>
 )
@@ -39,6 +44,24 @@ const ControlButtons = (): JSX.Element => {
     )
   }
 
+  const StepButton = (): JSX.Element => {
+    const isRunning = useSelector(selectIsRunning)
+
+    const handleClick = (): void => {
+      if (isRunning) {
+        return
+      }
+      void step()
+    }
+
+    return (
+      <ControlButton disabled={isRunning} onClick={handleClick}>
+        <Forward />
+        <span>Step</span>
+      </ControlButton>
+    )
+  }
+
   return (
     <>
       <ControlButton onClick={assemble}>
@@ -46,10 +69,7 @@ const ControlButtons = (): JSX.Element => {
         <span>Assemble</span>
       </ControlButton>
       <RunButton />
-      <ControlButton onClick={step}>
-        <Forward />
-        <span>Step</span>
-      </ControlButton>
+      <StepButton />
       <ControlButton onClick={reset}>
         <Undo />
         <span>Reset</span>
