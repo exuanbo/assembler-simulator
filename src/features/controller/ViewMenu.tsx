351d41adbf4c4aeef8f044ed6ced95c6ffaa3a77
exuanbo <exuanbo@protonmail.com>
2022-01-05 14:09:37 +0000

    refactor(ViewMenu): extract inlined component

--- src/features/controller/ViewMenu.tsx
+++ src/features/controller/ViewMenu.tsx
@@ -7,59 +7,55 @@ import { View, CheckMark } from '../../common/components/icons'
 import { useSelector, useDispatch } from '../../app/hooks'
 import { MemoryView, selectMemoryView, setMemoryView } from './controllerSlice'
 
-const ViewMenu = (): JSX.Element => {
+const MemoryMenu = (): JSX.Element => {
+  const memoryView = useSelector(selectMemoryView)
   const dispatch = useDispatch()
-
-  const MemoryMenu = (): JSX.Element => {
-    const memoryView = useSelector(selectMemoryView)
-
-    return (
-      <MenuItem.Expandable>
-        {(isHovered, menuItemsRef) => (
-          <>
-            <MenuButton>
-              <span className="w-4" />
-              <span>Memory</span>
-            </MenuButton>
-            {isHovered && (
-              <MenuItems.Expanded className="top-8" innerRef={menuItemsRef}>
-                {Object.values(MemoryView).map((memoryViewOption, index) => (
-                  <MenuItem
-                    key={index}
-                    onClick={() => {
-                      dispatch(setMemoryView(memoryViewOption))
-                    }}>
-                    <MenuButton>
-                      {memoryView === memoryViewOption ? <CheckMark /> : <span className="w-4" />}
-                      <span>{memoryViewOption}</span>
-                    </MenuButton>
-                  </MenuItem>
-                ))}
-              </MenuItems.Expanded>
-            )}
-          </>
-        )}
-      </MenuItem.Expandable>
-    )
-  }
-
   return (
-    <Menu>
-      {isOpen => (
+    <MenuItem.Expandable>
+      {(isHovered, menuItemsRef) => (
         <>
-          <MenuButton.Main>
-            <View />
-            <span>View</span>
-          </MenuButton.Main>
-          {isOpen && (
-            <MenuItems>
-              <MemoryMenu />
-            </MenuItems>
+          <MenuButton>
+            <span className="w-4" />
+            <span>Memory</span>
+          </MenuButton>
+          {isHovered && (
+            <MenuItems.Expanded className="top-8" innerRef={menuItemsRef}>
+              {Object.values(MemoryView).map((memoryViewOption, index) => (
+                <MenuItem
+                  key={index}
+                  onClick={() => {
+                    dispatch(setMemoryView(memoryViewOption))
+                  }}>
+                  <MenuButton>
+                    {memoryView === memoryViewOption ? <CheckMark /> : <span className="w-4" />}
+                    <span>{memoryViewOption}</span>
+                  </MenuButton>
+                </MenuItem>
+              ))}
+            </MenuItems.Expanded>
           )}
         </>
       )}
-    </Menu>
+    </MenuItem.Expandable>
   )
 }
+
+const ViewMenu = (): JSX.Element => (
+  <Menu>
+    {isOpen => (
+      <>
+        <MenuButton.Main>
+          <View />
+          <span>View</span>
+        </MenuButton.Main>
+        {isOpen && (
+          <MenuItems>
+            <MemoryMenu />
+          </MenuItems>
+        )}
+      </>
+    )}
+  </Menu>
+)
 
 export default ViewMenu
