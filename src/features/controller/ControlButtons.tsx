76c0bb118aa05df40eebcf94af0a4ec40330690e
exuanbo <exuanbo@protonmail.com>
2022-01-05 15:08:31 +0000

    refactor: re-order imports

--- src/features/controller/ControlButtons.tsx
+++ src/features/controller/ControlButtons.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react'
 import { Arrow, Play, Stop, Forward, Undo } from '../../common/components/icons'
-import { useSelector } from '../../app/hooks'
 import { useController } from './hooks'
 import { selectIsRunning } from './controllerSlice'
+import { useSelector } from '../../app/hooks'
 import { NO_BREAK_SPACE } from '../../common/constants'
 
 interface Props {
