9f3b52778be449bf67683fd58bbb589536d999e6
exuanbo <exuanbo@protonmail.com>
2022-01-15 16:26:32 +0000

    refactor(ToolBar): change z-index to 10

--- src/features/controller/Toolbar.tsx
+++ src/features/controller/Toolbar.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const ToolBar = ({ className }: Props): JSX.Element => (
   <div
-    className={`border-b flex flex-row-reverse min-w-max bg-gray-100 h-8 w-full z-2 fixed items-center justify-between ${className}`}>
+    className={`border-b flex flex-row-reverse min-w-max bg-gray-100 h-8 w-full z-10 fixed items-center justify-between ${className}`}>
     <div className="flex space-x-2 px-2 items-center">
       <h1 className="min-w-max">Assembler Simulator</h1>
       <a
