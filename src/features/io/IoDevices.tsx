73e79f59408f0231d9cd0babf149d8c92bd4fe50
exuanbo <exuanbo@protonmail.com>
2022-01-22 16:29:01 +0000

    refactor(IoDevices): rm icon

--- src/features/io/IoDevices.tsx
+++ src/features/io/IoDevices.tsx
@@ -3,35 +3,20 @@ import SimulatedKeyboard from './SimulatedKeyboard'
 import VisualDisplayUnit from './VisualDisplayUnit'
 import TrafficLights from './TrafficLights'
 import SevenSegmentDisplay from './SevenSegmentDisplay'
-import { EyeClosed, EyeOpen } from '@/common/components/icons'
-import { useToggle } from '@/common/hooks'
 
 interface Props {
   className?: string
 }
 
-const IoDevices = ({ className }: Props): JSX.Element => {
-  const [isActive, toggleActive] = useToggle(true)
-
-  return (
-    <Card
-      className={className}
-      icon={
-        <span className="flex items-center" onClick={toggleActive}>
-          {isActive ? <EyeClosed /> : <EyeOpen />}
-        </span>
-      }
-      title="I/O Devices">
-      <SimulatedKeyboard />
-      {isActive && (
-        <div className="flex flex-wrap py-1 px-1 items-start">
-          <VisualDisplayUnit />
-          <TrafficLights />
-          <SevenSegmentDisplay />
-        </div>
-      )}
-    </Card>
-  )
-}
+const IoDevices = ({ className }: Props): JSX.Element => (
+  <Card className={className} title="I/O Devices">
+    <SimulatedKeyboard />
+    <div className="flex flex-wrap py-1 px-1 items-start">
+      <VisualDisplayUnit />
+      <TrafficLights />
+      <SevenSegmentDisplay />
+    </div>
+  </Card>
+)
 
 export default IoDevices
