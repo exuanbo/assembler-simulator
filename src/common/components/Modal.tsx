5672e02c47df07d9db8c0c50c2ca86c17dfc59d1
exuanbo <exuanbo@protonmail.com>
2022-02-10 15:58:01 +0000

    refactor(Modal): use wrapper element

--- src/common/components/Modal.tsx
+++ src/common/components/Modal.tsx
@@ -1,5 +1,7 @@
-import type { ReactNode } from 'react'
+import { ReactNode, useState, useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
+
+const modalRoot = document.getElementById('modal-root')!
 
 interface Props {
   children: ReactNode
@@ -7,12 +9,29 @@ interface Props {
   className?: string
 }
 
-const Modal = ({ children, isOpen = false, className = '' }: Props): JSX.Element | null =>
-  isOpen
-    ? createPortal(
-        <div className={className}>{children}</div>,
-        document.getElementById('modal-root')!
-      )
-    : null
+const Modal = ({ children, isOpen = false, className = '' }: Props): JSX.Element | null => {
+  const wrapperElementRef = useRef<HTMLDivElement>()
+  const [isWrapperElementReady, setWrapperElementReady] = useState(false)
+
+  useEffect(() => {
+    if (!isOpen) {
+      return
+    }
+    if (wrapperElementRef.current === undefined) {
+      const wrapperElement = document.createElement('div')
+      wrapperElement.className = className
+      wrapperElementRef.current = wrapperElement
+    }
+    const { current: wrapperElement } = wrapperElementRef
+    modalRoot.appendChild(wrapperElement)
+    setWrapperElementReady(true)
+    return () => {
+      modalRoot.removeChild(wrapperElement)
+      setWrapperElementReady(false)
+    }
+  }, [isOpen])
+
+  return isOpen && isWrapperElementReady ? createPortal(children, wrapperElementRef.current!) : null
+}
 
 export default Modal
