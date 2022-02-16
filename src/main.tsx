07821522fa07dfa754c76644c96652387dec0605
exuanbo <exuanbo@protonmail.com>
2022-02-16 00:26:46 +0000

    refactor: rename `index.css` -> `styles.css`

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import 'virtual:windi.css'
-import './index.css'
+import './styles.css'
 import store from './app/store'
 import App from './app/App'
 
