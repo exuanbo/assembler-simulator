0cc938c2bac57fefdfcf8be2d19a4e46d9ab0ed4
exuanbo <exuanbo@protonmail.com>
2022-02-23 00:52:32 +0000

    revert: 3acf8cfebd24b42dead0090ea719efa0e22a3431 accidentally added unfinished `HelpMenu`

--- src/features/controller/HelpMenu.tsx
+++ src/features/controller/HelpMenu.tsx
@@ -1,12 +1,8 @@
-import { createContext, useContext, useCallback, useMemo } from 'react'
 import Menu from './Menu'
 import MenuButton from './MenuButton'
 import MenuItems from './MenuItems'
 import MenuItem from './MenuItem'
-import Modal from '@/common/components/Modal'
 import { Help } from '@/common/components/icons'
-import { useToggle, useOutsideClick } from '@/common/hooks'
-import { noop } from '@/common/utils'
 
 const InstructionSetButton = (): JSX.Element => (
   <MenuItem.ExternalLink href="http://www.softwareforeducation.com/sms32v50/sms32v50_manual/250-FullIset.htm">
@@ -26,91 +22,22 @@ const BugReportButton = (): JSX.Element => (
   </MenuItem.ExternalLink>
 )
 
-interface AboutModalContextValue {
-  isOpen: boolean
-  toggleOpen: () => void
-}
-
-const AboutModalContext = createContext<AboutModalContextValue>({
-  isOpen: false,
-  toggleOpen: noop
-})
-if (import.meta.env.DEV) {
-  AboutModalContext.displayName = 'AboutModalContext'
-}
-
-const AboutButton = (): JSX.Element => {
-  const { isOpen, toggleOpen } = useContext(AboutModalContext)
-
-  const handleClick = (): void => {
-    if (!isOpen) {
-      toggleOpen()
-    }
-  }
-
-  return (
-    <MenuItem onClick={handleClick}>
-      <MenuButton>
-        <span className="w-4" />
-        <span>About</span>
-      </MenuButton>
-    </MenuItem>
-  )
-}
-
-const AboutModal = (): JSX.Element => {
-  const { isOpen, toggleOpen } = useContext(AboutModalContext)
-
-  const handleOutsideClick = useCallback(() => {
-    if (isOpen) {
-      toggleOpen()
-    }
-  }, [isOpen])
-
-  const outsideClickRef = useOutsideClick(handleOutsideClick)
-
-  return (
-    <Modal isOpen={isOpen}>
-      <div className="flex inset-0 fixed items-center justify-center">
-        <div ref={outsideClickRef} className="bg-white">
-          About
-        </div>
-      </div>
-    </Modal>
-  )
-}
-
-const HelpMenu = (): JSX.Element => {
-  const [isAboutModalOpen, toggleAboutModalOpen] = useToggle(false)
-
-  const aboutModalContextValue = useMemo(() => {
-    return {
-      isOpen: isAboutModalOpen,
-      toggleOpen: toggleAboutModalOpen
-    }
-  }, [isAboutModalOpen])
-
-  return (
-    <Menu>
-      {(isOpen, hoverRef, menuElement) => (
-        <>
-          <MenuButton.Main innerRef={hoverRef}>
-            <Help />
-          </MenuButton.Main>
-          <AboutModalContext.Provider value={aboutModalContextValue}>
-            {isOpen && (
-              <MenuItems menuElement={menuElement}>
-                <InstructionSetButton />
-                <BugReportButton />
-                <AboutButton />
-              </MenuItems>
-            )}
-            <AboutModal />
-          </AboutModalContext.Provider>
-        </>
-      )}
-    </Menu>
-  )
-}
+const HelpMenu = (): JSX.Element => (
+  <Menu>
+    {(isOpen, hoverRef, menuElement) => (
+      <>
+        <MenuButton.Main innerRef={hoverRef}>
+          <Help />
+        </MenuButton.Main>
+        {isOpen && (
+          <MenuItems menuElement={menuElement}>
+            <InstructionSetButton />
+            <BugReportButton />
+          </MenuItems>
+        )}
+      </>
+    )}
+  </Menu>
+)
 
 export default HelpMenu

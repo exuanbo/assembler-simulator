exuanbo/assembler-simulator

commit 3acf8cfebd24b42dead0090ea719efa0e22a3431

refactor: rm `ListenAPI` and `WatchAPI`

src/features/controller/HelpMenu.tsx
@@ -1,8 +1,12 @@
+import { createContext, useContext, useCallback, useMemo } from 'react'
 import Menu from './Menu'
 import MenuButton from './MenuButton'
 import MenuItems from './MenuItems'
 import MenuItem from './MenuItem'
+import Modal from '@/common/components/Modal'
 import { Help } from '@/common/components/icons'
+import { useToggle, useOutsideClick } from '@/common/hooks'
+import { noop } from '@/common/utils'
 
 const InstructionSetButton = (): JSX.Element => (
   <MenuItem.ExternalLink href="http://www.softwareforeducation.com/sms32v50/sms32v50_manual/250-FullIset.htm">
@@ -22,22 +26,91 @@ const BugReportButton = (): JSX.Element => (
   </MenuItem.ExternalLink>
 )
 
-const HelpMenu = (): JSX.Element => (
-  <Menu>
-    {(isOpen, hoverRef, menuElement) => (
-      <>
-        <MenuButton.Main innerRef={hoverRef}>
-          <Help />
-        </MenuButton.Main>
-        {isOpen && (
-          <MenuItems menuElement={menuElement}>
-            <InstructionSetButton />
-            <BugReportButton />
-          </MenuItems>
-        )}
-      </>
-    )}
-  </Menu>
-)
+interface AboutModalContextValue {
+  isOpen: boolean
+  toggleOpen: () => void
+}
+
+const AboutModalContext = createContext<AboutModalContextValue>({
+  isOpen: false,
+  toggleOpen: noop
+})
+if (import.meta.env.DEV) {
+  AboutModalContext.displayName = 'AboutModalContext'
+}
+
+const AboutButton = (): JSX.Element => {
+  const { isOpen, toggleOpen } = useContext(AboutModalContext)
+
+  const handleClick = (): void => {
+    if (!isOpen) {
+      toggleOpen()
+    }
+  }
+
+  return (
+    <MenuItem onClick={handleClick}>
+      <MenuButton>
+        <span className="w-4" />
+        <span>About</span>
+      </MenuButton>
+    </MenuItem>
+  )
+}
+
+const AboutModal = (): JSX.Element => {
+  const { isOpen, toggleOpen } = useContext(AboutModalContext)
+
+  const handleOutsideClick = useCallback(() => {
+    if (isOpen) {
+      toggleOpen()
+    }
+  }, [isOpen])
+
+  const outsideClickRef = useOutsideClick(handleOutsideClick)
+
+  return (
+    <Modal isOpen={isOpen}>
+      <div className="flex inset-0 fixed items-center justify-center">
+        <div ref={outsideClickRef} className="bg-white">
+          About
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+const HelpMenu = (): JSX.Element => {
+  const [isAboutModalOpen, toggleAboutModalOpen] = useToggle(false)
+
+  const aboutModalContextValue = useMemo(() => {
+    return {
+      isOpen: isAboutModalOpen,
+      toggleOpen: toggleAboutModalOpen
+    }
+  }, [isAboutModalOpen])
+
+  return (
+    <Menu>
+      {(isOpen, hoverRef, menuElement) => (
+        <>
+          <MenuButton.Main innerRef={hoverRef}>
+            <Help />
+          </MenuButton.Main>
+          <AboutModalContext.Provider value={aboutModalContextValue}>
+            {isOpen && (
+              <MenuItems menuElement={menuElement}>
+                <InstructionSetButton />
+                <BugReportButton />
+                <AboutButton />
+              </MenuItems>
+            )}
+            <AboutModal />
+          </AboutModalContext.Provider>
+        </>
+      )}
+    </Menu>
+  )
+}
 
 export default HelpMenu

src/app/actionListener.ts
@@ -1,9 +1,6 @@
-import { Middleware, MiddlewareAPI, PayloadActionCreator, getType } from '@reduxjs/toolkit'
-import type { RootState, Dispatch } from './store'
+import { Middleware, PayloadActionCreator, getType } from '@reduxjs/toolkit'
 
-type ListenAPI = MiddlewareAPI<Dispatch, RootState>
-
-type ListenCallback<TPayload> = (payload: TPayload, api: ListenAPI) => void | Promise<void>
+type ListenCallback<TPayload> = (payload: TPayload) => void | Promise<void>
 
 interface ListenOptions {
   once?: boolean
@@ -29,7 +26,7 @@ export const createActionListener = (): ActionListener => {
 
   const actionListener: ActionListener = api => next => action => {
     const result = next(action)
-    subscriptions.get(action.type)?.forEach(cb => cb(action.payload, api))
+    subscriptions.get(action.type)?.forEach(cb => cb(action.payload))
     return result
   }
 
@@ -40,9 +37,9 @@ export const createActionListener = (): ActionListener => {
     }
     const callbacks = subscriptions.get(actionType)!
     const callback: typeof __callback = once
-      ? (payload, api) => {
+      ? payload => {
           unsubscribe()
-          return __callback(payload, api)
+          return __callback(payload)
         }
       : __callback
     callbacks.add(callback)

src/app/watcher.ts
@@ -1,14 +1,7 @@
-import type { Middleware, MiddlewareAPI } from '@reduxjs/toolkit'
-import type { RootState, Dispatch } from './store'
+import type { Middleware } from '@reduxjs/toolkit'
+import type { RootState } from './store'
 
-interface WatchAPI<TSelected> extends MiddlewareAPI<Dispatch, RootState> {
-  getPrevSelected: () => TSelected
-}
-
-type WatchCallback<TSelected> = (
-  selectedState: TSelected,
-  api: WatchAPI<TSelected>
-) => void | Promise<void>
+type WatchCallback<TSelected> = (selectedState: TSelected) => void | Promise<void>
 
 type Unsubscribe = () => void
 
@@ -46,12 +39,7 @@ export const createWatcher = (): Watcher => {
       const { prevSelected, callbacks } = subscription
       const selectedState = selector(state)
       if (selectedState !== prevSelected) {
-        callbacks.forEach(cb =>
-          cb(selectedState, {
-            ...api,
-            getPrevSelected: () => prevSelected
-          })
-        )
+        callbacks.forEach(cb => cb(selectedState))
         subscription.prevSelected = selectedState
       }
     })

src/features/controller/hooks.ts
@@ -380,7 +380,7 @@ export const useController = (): Controller => {
   const controller = useConstant(() => new Controller())
 
   useEffect(() => {
-    return watch(selectRuntimeConfiguration, async (_, { getState }) => {
+    return watch(selectRuntimeConfiguration, async () => {
       const state = getState()
       // `setSuspended` action listener will resume the main loop with the new configuration
       // so we skip calling `stopAndRun` if cpu is suspended

src/features/io/hooks.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 // TODO: remove batch from React 18
 import { batch } from 'react-redux'
 import { useSelector, useLazilyInitializedSelector } from '@/app/hooks'
-import { dispatch, listenAction } from '@/app/store'
+import { getState, dispatch, listenAction } from '@/app/store'
 import {
   IoDeviceName,
   IoDevice,
@@ -40,7 +40,7 @@ export const useVisualDisplayUnit = (): IoDevice => {
   const { data, isVisible, toggleVisible } = useIoDevice(IoDeviceName.VisualDisplayUnit)
 
   useEffect(() => {
-    return listenAction(setMemoryDataFrom, (_, { getState }) => {
+    return listenAction(setMemoryDataFrom, () => {
       const memoryData = selectMemoryData(getState())
       const vduData = getVduDataFrom(memoryData)
       const shouldToggleVisible = !isVisible && vduDataChanged(vduData)

commit 0cc938c2bac57fefdfcf8be2d19a4e46d9ab0ed4

revert: 3acf8cfebd24b42dead0090ea719efa0e22a3431 accidentally added unfinished `HelpMenu`

src/features/controller/HelpMenu.tsx
@@ -1,12 +1,8 @@
-import { createContext, useContext, useCallback, useMemo } from 'react'
 import Menu from './Menu'
 import MenuButton from './MenuButton'
 import MenuItems from './MenuItems'
 import MenuItem from './MenuItem'
-import Modal from '@/common/components/Modal'
 import { Help } from '@/common/components/icons'
-import { useToggle, useOutsideClick } from '@/common/hooks'
-import { noop } from '@/common/utils'
 
 const InstructionSetButton = (): JSX.Element => (
   <MenuItem.ExternalLink href="http://www.softwareforeducation.com/sms32v50/sms32v50_manual/250-FullIset.htm">
@@ -26,91 +22,22 @@ const BugReportButton = (): JSX.Element => (
   </MenuItem.ExternalLink>
 )
 
-interface AboutModalContextValue {
-  isOpen: boolean
-  toggleOpen: () => void
-}
-
-const AboutModalContext = createContext<AboutModalContextValue>({
-  isOpen: false,
-  toggleOpen: noop
-})
-if (import.meta.env.DEV) {
-  AboutModalContext.displayName = 'AboutModalContext'
-}
-
-const AboutButton = (): JSX.Element => {
-  const { isOpen, toggleOpen } = useContext(AboutModalContext)
-
-  const handleClick = (): void => {
-    if (!isOpen) {
-      toggleOpen()
-    }
-  }
-
-  return (
-    <MenuItem onClick={handleClick}>
-      <MenuButton>
-        <span className="w-4" />
-        <span>About</span>
-      </MenuButton>
-    </MenuItem>
-  )
-}
-
-const AboutModal = (): JSX.Element => {
-  const { isOpen, toggleOpen } = useContext(AboutModalContext)
-
-  const handleOutsideClick = useCallback(() => {
-    if (isOpen) {
-      toggleOpen()
-    }
-  }, [isOpen])
-
-  const outsideClickRef = useOutsideClick(handleOutsideClick)
-
-  return (
-    <Modal isOpen={isOpen}>
-      <div className="flex inset-0 fixed items-center justify-center">
-        <div ref={outsideClickRef} className="bg-white">
-          About
-        </div>
-      </div>
-    </Modal>
-  )
-}
-
-const HelpMenu = (): JSX.Element => {
-  const [isAboutModalOpen, toggleAboutModalOpen] = useToggle(false)
-
-  const aboutModalContextValue = useMemo(() => {
-    return {
-      isOpen: isAboutModalOpen,
-      toggleOpen: toggleAboutModalOpen
-    }
-  }, [isAboutModalOpen])
-
-  return (
-    <Menu>
-      {(isOpen, hoverRef, menuElement) => (
-        <>
-          <MenuButton.Main innerRef={hoverRef}>
-            <Help />
-          </MenuButton.Main>
-          <AboutModalContext.Provider value={aboutModalContextValue}>
-            {isOpen && (
-              <MenuItems menuElement={menuElement}>
-                <InstructionSetButton />
-                <BugReportButton />
-                <AboutButton />
-              </MenuItems>
-            )}
-            <AboutModal />
-          </AboutModalContext.Provider>
-        </>
-      )}
-    </Menu>
-  )
-}
+const HelpMenu = (): JSX.Element => (
+  <Menu>
+    {(isOpen, hoverRef, menuElement) => (
+      <>
+        <MenuButton.Main innerRef={hoverRef}>
+          <Help />
+        </MenuButton.Main>
+        {isOpen && (
+          <MenuItems menuElement={menuElement}>
+            <InstructionSetButton />
+            <BugReportButton />
+          </MenuItems>
+        )}
+      </>
+    )}
+  </Menu>
+)
 
 export default HelpMenu