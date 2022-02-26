8f06d23e0b1b287a7cace7b80bfff821991e1cee
exuanbo <exuanbo@protonmail.com>
2022-02-26 11:49:11 +0000

    refactor(app/hooks): `useLazilyInitializedSelector` use `useMemo`

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,6 @@
+import { useMemo } from 'react'
 import { TypedUseSelectorHook, useSelector as __useSelector } from 'react-redux'
 import type { RootState } from './store'
-import { useConstant } from '@/common/hooks'
 
 export const useSelector: TypedUseSelectorHook<RootState> = __useSelector
 
@@ -8,6 +8,6 @@ export const useLazilyInitializedSelector = <TSelected>(
   selectorFactory: () => (state: RootState) => TSelected,
   equalityFn?: (left: TSelected, right: TSelected) => boolean
 ): TSelected => {
-  const selector = useConstant(selectorFactory)
+  const selector = useMemo(selectorFactory, [])
   return useSelector(selector, equalityFn)
 }
