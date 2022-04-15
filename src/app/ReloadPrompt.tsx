66cecc45e8fea237a6de2c05e8ee6c5b297a5d81
exuanbo <exuanbo@protonmail.com>
2022-04-15 13:49:44 +0100

    refactor(ReloadPrompt): change background color

--- src/app/ReloadPrompt.tsx
+++ src/app/ReloadPrompt.tsx
@@ -1,6 +1,6 @@
 import { useRegisterSW } from 'virtual:pwa-register/react'
 
-const buttonClassName = 'border rounded py-1 px-2 inline-block hover:bg-gray-200'
+const buttonClassName = 'border rounded bg-gray-100 py-1 px-2 inline-block hover:bg-gray-200'
 
 const ReloadPrompt = (): JSX.Element | null => {
   const {
@@ -18,7 +18,7 @@ const ReloadPrompt = (): JSX.Element | null => {
   }
 
   return needReload ? (
-    <div className="bg-white border rounded space-y-2 shadow m-4 py-2 px-4 right-0 bottom-0 fixed">
+    <div className="border rounded space-y-2 bg-light-100 shadow m-4 py-2 px-4 right-0 bottom-0 fixed">
       <div>New version is available</div>
       <div className="space-x-2">
         <div className={buttonClassName} onClick={handleClickReload}>
