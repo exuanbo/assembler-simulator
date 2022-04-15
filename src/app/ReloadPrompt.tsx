85e50b99dcb4adc15e59caeb3adea33310f95c12
exuanbo <exuanbo@protonmail.com>
2022-04-15 14:02:07 +0100

    feat: add spinner after click `Reload`

--- src/app/ReloadPrompt.tsx
+++ src/app/ReloadPrompt.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { useRegisterSW } from 'virtual:pwa-register/react'
+import { Spinner } from '@/common/components/icons'
 
 const buttonClassName = 'border rounded bg-gray-100 py-1 px-2 inline-block hover:bg-gray-200'
 
@@ -8,8 +10,10 @@ const ReloadPrompt = (): JSX.Element | null => {
     updateServiceWorker
   } = useRegisterSW({ immediate: true })
 
+  const [isReloading, setReloading] = useState(false)
+
   const handleClickReload = async (): Promise<void> => {
-    setNeedReload(false)
+    setReloading(true)
     await updateServiceWorker(/* reloadPage: */ true)
   }
 
@@ -17,7 +21,11 @@ const ReloadPrompt = (): JSX.Element | null => {
     setNeedReload(false)
   }
 
-  return needReload ? (
+  return isReloading ? (
+    <div className="bg-white flex bg-opacity-80 inset-0 z-10 fixed justify-center items-center">
+      <Spinner className="animate-spin w-6" />
+    </div>
+  ) : needReload ? (
     <div className="border rounded space-y-2 bg-light-100 shadow m-4 py-2 px-4 right-0 bottom-0 fixed">
       <div>New version is available</div>
       <div className="space-x-2">
