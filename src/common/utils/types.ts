f9fb80b61fbf6932e302fbea59ee023792f5f8f0
exuanbo <exuanbo@protonmail.com>
2022-04-20 00:04:08 +0100

    fix(common/utils): `ExpandDeep` with symbol as key

--- src/common/utils/types.ts
+++ src/common/utils/types.ts
@@ -1,7 +1,7 @@
 // Modified from an answer to the question
 // "How can I see the full expanded contract of a Typescript type?"
 // https://stackoverflow.com/a/57683652/13346012
-export type ExpandDeep<T> = T extends Record<string, unknown>
+export type ExpandDeep<T> = T extends Record<string | number | symbol, unknown>
   ? { [K in keyof T]: ExpandDeep<T[K]> }
   : T extends Array<infer E>
   ? Array<ExpandDeep<E>>
