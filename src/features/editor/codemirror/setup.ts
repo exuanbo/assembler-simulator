42fe9646f4fa3f284172b6d034e1e4d5319fc8b8
exuanbo <exuanbo@protonmail.com>
2022-04-21 14:34:12 +0100

    refactor(codemirror/setup): reorder extensions

--- src/features/editor/codemirror/setup.ts
+++ src/features/editor/codemirror/setup.ts
@@ -3,22 +3,22 @@ import { keymap, drawSelection } from '@codemirror/view'
 import { history, defaultKeymap, historyKeymap } from '@codemirror/commands'
 import { closeBrackets, closeBracketsKeymap } from '@codemirror/autocomplete'
 import { theme } from './theme'
-import { wavyUnderline } from './wavyUnderline'
-import { highlightLine } from './highlightLine'
 import { asm } from './lang-asm'
 import { breakpoints } from './breakpoints'
 import { lineNumbers } from './lineNumbers'
+import { wavyUnderline } from './wavyUnderline'
+import { highlightLine } from './highlightLine'
 import { indentWithTab } from './indentWithTab'
 
 export const setup: Extension = [
-  theme(),
   drawSelection(),
-  wavyUnderline(),
-  highlightLine(),
-  asm(),
   history(),
+  closeBrackets(),
+  theme(),
+  asm(),
   breakpoints(),
   lineNumbers(),
-  closeBrackets(),
+  wavyUnderline(),
+  highlightLine(),
   keymap.of([...closeBracketsKeymap, ...defaultKeymap, ...historyKeymap, indentWithTab])
 ]
