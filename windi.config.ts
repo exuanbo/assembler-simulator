4426434a237feacc13a402b4ce0fa1a65b17d1b6
exuanbo <exuanbo@protonmail.com>
2022-04-23 00:26:27 +0100

    build(windi): add `blocklist`, narrow `fontFamily`

--- windi.config.ts
+++ windi.config.ts
@@ -4,12 +4,17 @@ export default defineConfig({
   extract: {
     include: ['index.html', 'src/**/*.tsx']
   },
+  preflight: {
+    blocklist: ['input']
+  },
+  blocklist: ['container'],
   theme: {
     extend: {
       cursor: {
         'col-resize': 'col-resize'
       },
       fontFamily: {
+        sans: ['sans-serif'],
         mono: ['Jetbrains Mono', 'monospace']
       }
     }
