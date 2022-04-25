b40bc7f09a24f1b1aff0cfbcaa0d3059bbe24bb2
exuanbo <exuanbo@protonmail.com>
2022-04-25 23:42:23 +0100

    refactor(Anchor): inline function

--- src/common/components/Anchor.tsx
+++ src/common/components/Anchor.tsx
@@ -1,24 +1,25 @@
 import type { ReactNode } from 'react'
 
-const isExternal = (href: string): boolean => href.startsWith('http')
-
 interface Props {
   href: string
   children: ReactNode
 }
 
-const Anchor = ({ href, children }: Props): JSX.Element => (
-  <a
-    className="text-blue-700 hover:underline"
-    href={href}
-    {...(isExternal(href)
-      ? {
-          rel: 'noopener noreferrer',
-          target: '_blank'
-        }
-      : undefined)}>
-    {children}
-  </a>
-)
+const Anchor = ({ href, children }: Props): JSX.Element => {
+  const isExternal = href.startsWith('http')
+  return (
+    <a
+      className="text-blue-700 hover:underline"
+      href={href}
+      {...(isExternal
+        ? {
+            rel: 'noopener noreferrer',
+            target: '_blank'
+          }
+        : undefined)}>
+      {children}
+    </a>
+  )
+}
 
 export default Anchor
