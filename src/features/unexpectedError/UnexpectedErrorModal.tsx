dcfc4be2293e1326de06b6cb75fa490e2cf6af77
exuanbo <exuanbo@protonmail.com>
2022-04-26 01:37:07 +0100

    refactor(UnexpectedErrorModal): simplify condition

--- src/features/unexpectedError/UnexpectedErrorModal.tsx
+++ src/features/unexpectedError/UnexpectedErrorModal.tsx
@@ -26,31 +26,29 @@ const UnexpectedErrorModal = (): JSX.Element => {
         {hasUnexpectedError
           ? unexpectedError.stack
               ?.split(/\n(.*)/s)
-              .slice(0, -1)
+              .slice(0, -1) // remove empty string
               .map((line, lineIndex) => {
                 const isErrorMessage = lineIndex === 0
-                return (
+                return isErrorMessage ? (
+                  <div key={lineIndex}>
+                    {line.split(': ').map((messagePart, messagePartIndex) => {
+                      const isErrorName = messagePartIndex === 0
+                      return (
+                        <div
+                          key={messagePartIndex}
+                          className={
+                            isErrorName ? 'text-xl text-red-500' : 'border-b text-lg py-1'
+                          }>
+                          {messagePart}
+                        </div>
+                      )
+                    })}
+                  </div>
+                ) : (
                   <div
                     key={lineIndex}
-                    className={
-                      isErrorMessage
-                        ? undefined
-                        : 'rounded bg-red-50 py-2 px-5.5 text-gray-600 whitespace-pre-line'
-                    }>
-                    {isErrorMessage
-                      ? line.split(': ').map((messagePart, messagePartIndex) => {
-                          const isErrorName = messagePartIndex === 0
-                          return (
-                            <div
-                              key={messagePartIndex}
-                              className={
-                                isErrorName ? 'text-xl text-red-500' : 'border-b text-lg py-1'
-                              }>
-                              {messagePart}
-                            </div>
-                          )
-                        })
-                      : line}
+                    className="rounded bg-red-50 py-2 px-5.5 text-gray-600 whitespace-pre-line">
+                    {line}
                   </div>
                 )
               })
