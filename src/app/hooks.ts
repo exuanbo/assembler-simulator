c52af9a92655316994cfab9fd8f974206c4b5877
exuanbo <exuanbo@protonmail.com>
2022-05-12 01:41:52 +0100

    chore(app/hooks): rm unused hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -4,18 +4,9 @@ import {
   useSelector as __useSelector
 } from 'react-redux'
 import type { RootState, Store } from './store'
-import { useConstant } from '@/common/hooks'
 
 type TypedUseStoreHook = () => Store
 
 export const useStore: TypedUseStoreHook = __useStore
 
 export const useSelector: TypedUseSelectorHook<RootState> = __useSelector
-
-export const useLazilyInitializedSelector = <TSelected>(
-  selectorFactory: () => (state: RootState) => TSelected,
-  equalityFn?: (left: TSelected, right: TSelected) => boolean
-): TSelected => {
-  const selector = useConstant(selectorFactory)
-  return useSelector(selector, equalityFn)
-}
