4b9bcd0bc433156a47aa7e0c2629e22b78468d79
exuanbo <exuanbo@protonmail.com>
2022-08-08 19:04:49 +0100

    refactor(codemirror/lineNumbers): extract gutter dom event handler

--- src/features/editor/codemirror/lineNumbers.ts
+++ src/features/editor/codemirror/lineNumbers.ts
@@ -1,19 +1,22 @@
 import type { Extension } from '@codemirror/state'
 import { EditorView, lineNumbers as __lineNumbers } from '@codemirror/view'
 import { toggleBreakpointOnMouseEvent } from './breakpoints'
+import type { DOMEventHandler as GutterDOMEventHandler } from './gutter'
+
+const toggleBreakpointOnStrictMouseEvent: GutterDOMEventHandler = (view, line, event) => {
+  const { target } = event
+  // only when clicking on a gutter element
+  if (target instanceof Element && target.classList.contains('cm-gutterElement')) {
+    return toggleBreakpointOnMouseEvent(view, line, event)
+  }
+  return false
+}
 
 export const lineNumbers = (): Extension => {
   return [
     __lineNumbers({
       domEventHandlers: {
-        // TODO: extract
-        mousedown: (view, line, event) => {
-          const { target } = event
-          if (target instanceof Element && target.classList.contains('cm-gutterElement')) {
-            return toggleBreakpointOnMouseEvent(view, line, event)
-          }
-          return false
-        }
+        mousedown: toggleBreakpointOnStrictMouseEvent
       }
     }),
     EditorView.baseTheme({
