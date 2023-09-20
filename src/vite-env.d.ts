491aa99fe01b755a929b31351c723221c2377d0d
exuanbo <exuanbo@protonmail.com>
2023-09-20 21:27:51 +0100

    chore(deps): bump versions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite-plugin-pwa/react" />
 
 declare const __VERSION__: string
 declare const __COMMIT_HASH__: string
