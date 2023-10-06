e25ff792e58b93ed5c72531f96e6207015f4d7d4
exuanbo <exuanbo@protonmail.com>
2023-10-06 22:35:16 +0100

    chore: use tsconfig.eslint.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['./node_modules/ts-standardx/.eslintrc.js'],
+  parserOptions: {
+    project: './tsconfig.eslint.json'
+  },
   rules: {
     'react/jsx-handler-names': 'off',
     'react/jsx-sort-props': [
