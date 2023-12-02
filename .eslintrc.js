500e18548d9db722633a06050e2722f1c3b797e1
exuanbo <exuanbo@protonmail.com>
2023-12-02 01:50:50 +0000

    chore: rm ts-standardx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,69 @@
-module.exports = {
-  extends: ['./node_modules/ts-standardx/.eslintrc.js'],
+// @ts-check
+/** @typedef {import('eslint').Linter.Config} Config */
+
+/** @type {Config} */
+const defaultConfig = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['*.?(c|m)ts?(x)'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+        extraFileExtensions: ['.cts', '.mts'],
+      },
+      rules: {
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+      },
+    },
+  ],
   parserOptions: {
-    project: './tsconfig.eslint.json'
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['simple-import-sort'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   rules: {
-    'react/jsx-handler-names': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/prop-types': 'off',
     'react/jsx-sort-props': [
       'error',
       {
         callbacksLast: true,
         shorthandFirst: true,
-        reservedFirst: true
-      }
-    ]
-  }
+        reservedFirst: true,
+      },
+    ],
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '(useViewEffect)',
+      },
+    ],
+  },
 }
+
+module.exports = defaultConfig
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,10 @@
+// @ts-check
+/** @typedef {import('prettier').Config} Config */
+
+/** @type {Config} */
 module.exports = {
-  printWidth: 100
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
 }
