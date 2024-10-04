9b4214169af7a9ca20ad02b6acbaec91718ca5ab
exuanbo <exuanbo@protonmail.com>
2024-10-04 21:48:28 +0100

    fix(core): patch rxjs

--- src/rxjs.d.ts
+++
@@ -1,6 +0,0 @@
-import type { MonoTypeOperatorFunction } from 'rxjs'
-
-declare module 'rxjs' {
-  export function filter<T>(predicate: (value: T, index: number) => unknown): MonoTypeOperatorFunction<T>
-  export function skipWhile<T>(predicate: (value: T, index: number) => unknown): MonoTypeOperatorFunction<T>
-}
