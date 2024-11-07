fef49f957e7a917073007dffe98b580a3bd4855b
exuanbo <exuanbo@protonmail.com>
2024-11-07 10:56:00 +0000

    fix(core): handle step result

--- src/core/controller/controller.ts
+++ src/core/controller/controller.ts
@@ -1,6 +1,6 @@
 import { Observable, type Subscription } from 'rxjs'
 
-import type { Bus, ControlLines } from '../bus/bus'
+import type { Bus } from '../bus/bus'
 import type { Clock } from '../clock/clock'
 import type { Cpu } from '../cpu/cpu'
 import type { Memory } from '../memory/memory'
@@ -16,22 +16,23 @@ export class Controller {
   step = (): Observable<void> => {
     return new Observable<void>((subscriber) => {
       const step = this.cpu.step()
-      let subscription: Subscription
+      let subscription: Subscription | undefined
 
-      const handleResult = (result: IteratorResult<Observable<ControlLines>, void>) => {
+      const handleResult = (result: ReturnType<typeof step.next>) => {
         if (result.done) {
           subscriber.next()
           subscriber.complete()
           return
         }
         queueMicrotask(this.clock.tick)
+        subscription?.unsubscribe()
         subscription = result.value.subscribe((signals) => {
           handleResult(step.next(signals))
         })
       }
 
       handleResult(step.next())
-      return () => subscription.unsubscribe()
+      return () => subscription?.unsubscribe()
     })
   }
 }
