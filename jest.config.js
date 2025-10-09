17effcbe5a32a6b0f9c4006b2396c793637e7559
exuanbo <exuanbo@protonmail.com>
2025-10-09 10:05:38 +0100

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -9,22 +9,13 @@ const config = {
     '^@/(.*)': '<rootDir>/src/$1',
     '(.+)\\?raw$': '$1',
   },
-  // TODO: remove once Jest supports Prettier version 3
-  // https://jestjs.io/docs/configuration#prettierpath-string
-  prettierPath: null,
   snapshotFormat: {
     escapeString: true,
   },
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/__tests__/**/*.test.ts?(x)'],
   transform: {
-    '\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.test.json',
-        isolatedModules: true,
-      },
-    ],
+    '\\.tsx?$': 'ts-jest',
     '\\.asm$': '<rootDir>/__tests__/rawTransformer.js',
   },
 }
