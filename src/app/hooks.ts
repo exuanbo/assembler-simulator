401d371ea5f878bacb079b36404798ce8393fe44
exuanbo <exuanbo@protonmail.com>
2025-10-20 00:34:13 +0100

    refactor(app/hooks): simplify `useAckee`

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,23 +1,19 @@
 import * as tracker from 'ackee-tracker'
 import { useEffect } from 'react'
 
-import { useSingleton } from '@/common/hooks'
-
 const DETAILED = true
 
 export const useAckee = () => {
-  const instance = useSingleton(() => tracker.create('https://ackee.exuanbo.xyz/'))
-
   useEffect(() => {
-    const attributes = tracker.attributes(DETAILED)
-
     const { pathname, origin } = window.location
     const url = new URL(pathname, origin)
 
-    const { stop } = instance.record('bc75fd47-884f-4723-aaf6-3384103e0095', {
-      ...attributes,
-      siteLocation: url.href,
-    })
+    const { stop } = tracker
+      .create('https://ackee.exuanbo.xyz/')
+      .record('bc75fd47-884f-4723-aaf6-3384103e0095', {
+        ...tracker.attributes(DETAILED),
+        siteLocation: url.href,
+      })
     return stop
-  }, [instance])
+  }, [])
 }
